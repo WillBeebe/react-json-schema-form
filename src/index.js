4dd9405103ab96ae0d3117b06fc385fa3929a10c
Will <651833+WillBeebe@users.noreply.github.com>
2024-09-03 12:51:07 -0600

    check in initial component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as FieldRenderer } from './components/FieldRenderer';
+export { default as JsonSchemaForm } from './components/JsonSchemaForm';
+// Export other components or utilities as needed
+
+// import JsonSchemaForm from './JsonSchemaForm';
+// import GCPRegionsComponent from './customComponents/GCPRegions';
+// export default JsonSchemaForm;
+// export { GCPRegionsComponent };
